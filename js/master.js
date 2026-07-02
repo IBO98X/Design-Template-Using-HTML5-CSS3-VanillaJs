785237ee05201cec92330cc40d494fabafc9096b
IBO98X <ibrahim.qahtan919@gmail.com>
2026-07-02 22:21:11 +0300

    Upload new progress of settings box

--- js/master.js
+++ js/master.js
@@ -1,3 +1,11 @@
+//Toggle Sping Class On Icon
+document.querySelector(".toggle-settings .fa-gear").onclick = function () {
+  // Toggle Class Fa-spin For Rotation On Self
+  this.classList.toggle("fa-spin");
+  // Toggle Class Open On Main Settings Box
+  document.querySelector(".settings-box").classList.toggle("open");
+};
+
 // Select Landing Page Element
 let landingPage = document.querySelector(".landing-page");
 
