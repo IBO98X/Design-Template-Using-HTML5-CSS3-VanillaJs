64f16b12396868a3d366efc6913f868775be31b2
IBO98X <ibrahim.qahtan919@gmail.com>
2026-07-04 22:01:40 +0300

    Upload new progress [random backgrounds yes or no]

--- js/master.js
+++ js/master.js
@@ -16,6 +16,12 @@ if (mainColors !== null) {
   });
 }
 
+// Random Background Option
+let backgroundOption = true;
+
+// Variable To Control The Interval
+let theBackgroundInterval;
+
 //Toggle Sping Class On Icon
 document.querySelector(".toggle-settings .fa-gear").onclick = function () {
   // Toggle Class Fa-spin For Rotation On Self
@@ -47,6 +53,30 @@ colorsLi.forEach((li) => {
   });
 });
 
+// Switch Random Backgrounds Option
+const randomBackgroundsElement = document.querySelectorAll(
+  ".random-backgrounds",
+);
+// Loop On All Spans
+randomBackgroundsElement.forEach((li) => {
+  // Click On Every List Items
+  li.addEventListener("click", (e) => {
+    // Remove Active Class From All Spans
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
+    });
+    // Add Active Class On Self
+    e.target.classList.add("active");
+    if (e.target.dataset.background === "yes") {
+      backgroundOption = true;
+      randomizeImgs();
+    } else {
+      backgroundOption = false;
+      clearInterval(theBackgroundInterval);
+    }
+  });
+});
+
 // Select Landing Page Element
 let landingPage = document.querySelector(".landing-page");
 
@@ -59,11 +89,17 @@ let imgsArray = [
   "Landing5.jpg",
 ];
 
-setInterval(() => {
-  // Get Random Number
-  let randomNumber = Math.floor(Math.random() * imgsArray.length);
+// Function To Randomize Imgs
+function randomizeImgs() {
+  if (backgroundOption === true) {
+    theBackgroundInterval = setInterval(() => {
+      // Get Random Number
+      let randomNumber = Math.floor(Math.random() * imgsArray.length);
 
-  // Change Background Image Url
-  landingPage.style.backgroundImage =
-    'url("imgs/' + imgsArray[randomNumber] + '")';
-}, 10000);
+      // Change Background Image Url
+      landingPage.style.backgroundImage =
+        'url("imgs/' + imgsArray[randomNumber] + '")';
+    }, 10000);
+  }
+}
+randomizeImgs();
